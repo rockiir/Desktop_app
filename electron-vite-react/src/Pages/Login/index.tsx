532063dfa36ec16b0b2494f230832842907724f8
rockiir <70121094+rockiir@users.noreply.github.com>
2022-09-24 20:50:48 -0300

    base

---
+++ electron-vite-react/src/Pages/Login/index.tsx
@@ -0,0 +1,37 @@
+
+import { useEffect, useState } from 'react';
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const ButtonExampleButton = () => <Button>Click Here</Button>
+
+function Login() {
+
+    return (
+
+        <>
+
+  <link
+    async
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+  />
+  <script src="https://cdn.jsdelivr.net/npm/semantic-ui-react/dist/umd/semantic-ui-react.min.js"></script>
+
+  <script src="like_button.js"></script>
+
+  <button class="ui button" style={{backgroundColor: "red"}}>oi Here</button>
+
+<h1>hello</h1>
+
+        </>
+            )
+        }
+
+
+
+
+
+
+
+export default Login
